c57dfcbb050f021fc7f65b8a8d2253e248b9eb9c
ReyesMeng <oucxuxu1998@outlook.com>
2019-09-11 10:01:14 +0800

    hexo

--- scripts/openeditor.js
+++ scripts/openeditor.js
@@ -1,11 +1,11 @@
 //Windows
-//var spawn = require('child_process').exec;
-//hexo.on('new', function(data){
-//	spawn('start  "~/atom" ' + data.path);
-//});
-
-//Linux
 var spawn = require('child_process').exec;
 hexo.on('new', function(data){
-	spawn('atom',[data.path]);
+	spawn('start  "D:\\software\\Notepad++\\notepad++.exe" ' + data.path);
 });
+
+//Linux
+//var spawn = require('child_process').exec;
+//hexo.on('new', function(data){
+//	spawn('atom',[data.path]);
+//});
